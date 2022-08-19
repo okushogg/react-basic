6f62ee748ccddd3fe4025466e694c6acf0b744dc
Shota Okugawa <okusho721@gmail.com>
2022-08-19 15:06:54 +0700

    S9 use API with useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 // import TextInput from './components/TextInput';
 // import Article from "./components/Article";
 // import Counter from './components/Counter';
-import {Counter,ToggleButton} from './components';
+// import {Counter,ToggleButton} from './components';
 
 
 function App(){
+  const [name, setName] =useState('')
+  const [id, setId] = useState('okushogg')
+  const ids = ['okushogg', 'gaearon', 'aws', 'google', 'facebook']
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)]
+    setId(_id)
+  }
+
+  useEffect(()=>{
+    fetch(`https://api.github.com/users/${id}`)
+    .then(res => res.json())
+    .then(data=>{
+      console.log(data)
+      setName(data.name)
+    })
+    .catch(error =>{
+      console.error(error)
+    })
+  })
   return (
     <div>
-      <Counter />
-      {/* <ToggleButton /> */}
+      <p>{id}の、GitHub上の名前は{name}です。</p>
+      <button onClick={getRandomId}>IDを変更</button>
     </div>
   );
 }
